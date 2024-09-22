89652c07b0a153f45509758cdcf9c977a01a7e27
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-09-22 11:44:28 +0200

    documented main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -10,25 +10,30 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    // If we didnt specify the file we quit
     if (argc <= 1) {
         cout << "No files specified. Quitting..." << endl;
         return 0;
     }
 
+    // Reading the file
     file* f = new file(argv[1]);
     int errcode = f->read();
 
+    // If something went wrong
     if (errcode) {
         cout << "Error " << errcode << " occured" << endl;
         return 0;
     }
 
+    // Set the window name as the file name
     cout << "\033]0;" << f->fullname << "\007";
 
+    // Init ncurses
     WINDOW* window = initscr();
-    keypad(window, true);
+    keypad(window, true); // Arrow keys
     nodelay(window, true);
-    scrollok(window, false);
+    scrollok(window, false); // Disable cursor going of of the screen
     noecho();
 
     // Colors
@@ -40,6 +45,7 @@ int main(int argc, char* argv[]) {
 
     bool running = true;
 
+    // Initializing input
     editor::input input = editor::input(f, window, 0, 0);
 
     while (running) {
@@ -59,14 +65,18 @@ int main(int argc, char* argv[]) {
         // Top bar
         {
             string bar = std::format("{}", string(getmaxx(window), ' '));
+
+            // Bar components
             string cursor = std::format(" {}:{} ", yCur + 1, xCur + 1);
             string file = std::format(" {} ", f->fullname);
 
+            // No background
             attron(COLOR_PAIR(1));
             mvaddstr(0, 0, bar.c_str());
             mvaddstr(0, cursor.size(), file.c_str());
             attroff(COLOR_PAIR(1));
 
+            // With background
             attron(COLOR_PAIR(2));
             mvaddstr(0, 0, cursor.c_str());
             attroff(COLOR_PAIR(2));
@@ -74,43 +84,62 @@ int main(int argc, char* argv[]) {
 
         // Editor
         {
+            // Max margin for x
             int maxMargin = utils::toString(f->contents.size()).size();
 
             int page = input.getPageY();
 
+            // Relative cursor position
             int xC = 0;
             int yC = 0;
 
             editor::mode mode = input.getMode();
 
             for (int i = page; i < f->contents.size(); i++) {
+                // Line number
                 int j = i + 1;
 
-                attron(COLOR_PAIR(3));
+                // Line number with margin
                 string spaces = string(maxMargin + 1 - utils::toString(j).size(), ' ');
                 string ln = std::format("{}{} ", spaces, j);
 
+                // Text line
+                string line = std::format(" {}", f->contents.at(i).c_str());
+
+                // Printing line
+
+                attron(COLOR_PAIR(3));
                 mvaddstr(j - page, 0, ln.c_str());
                 attroff(COLOR_PAIR(3));
 
                 use_default_colors();
-                string line = std::format(" {}", f->contents.at(i).c_str());
                 mvaddstr(j - page, maxMargin + 2, line.c_str());
             }
 
             if (mode == editor::COMMAND) {
+                // Propmt of COMMAND mode
+                string prompt = std::format("> {}", string(getmaxx(window) - 2, ' '));
+                // Current input
+                string contents = utils::join(input.getCommand(), " ");
+
+                // Printing
                 attron(COLOR_PAIR(3));
-                mvaddstr(getmaxy(window) - 1, 0, std::format("> {}", string(getmaxx(window) - 2, ' ')).c_str());
-                mvaddstr(getmaxy(window) - 1, 2, utils::join(input.getCommand(), " ").c_str());
+                mvaddstr(getmaxy(window) - 1, 0, prompt.c_str());
+                mvaddstr(getmaxy(window) - 1, 2, contents.c_str());
                 attroff(COLOR_PAIR(3));
 
-                xC = 2 + input.getCommandX();
+                // Cursor Y
                 yC = getmaxy(window) - 1;
 
+                // If returned then execure
                 if (input.isCommandReturned()) {
                     editor::commandHandler::execute(input.getCommand(), &input, running);
-                    input.clearCommand();
+                    input.clearCommand(); // also resets cursor X
+                    return;
                 }
+
+                // Cursor X
+                xC = 2 + input.getCommandX();
             }
 
             if (mode == editor::EDITING) {
